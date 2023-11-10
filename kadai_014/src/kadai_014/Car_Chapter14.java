2aa63eb48bd87076de6cb064d469236f557878bd
makinokeisuke <makino.19820629@gmail.com>
2023-11-10 23:16:10 +0900

    フォーコミット

---
+++ kadai_014/src/kadai_014/Car_Chapter14.java
@@ -0,0 +1,23 @@
+package kadai_014;
+
+public class Car_Chapter14 {
+	
+	private int gear = 1;
+	private int speed = 10;
+	
+	public void gearChange(int afterGear) {
+		this.speed = switch(afterGear) {
+			case 1 -> 10;
+			case 2 -> 20;
+			case 3 -> 30;
+			case 4 -> 40;
+			case 5 -> 50;
+			default -> 10;
+	};
+		this.gear = afterGear;
+	}
+	public void run() {
+		System.out.println("ギア1から" + this.gear + "までに切り替えました");
+		System.out.println("速度は時速" + speed + "kmです");
+	}
+}
